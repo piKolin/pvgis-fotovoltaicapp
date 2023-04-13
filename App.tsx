06b204a34cc2c16aea1c019c879aae0d7bbcf62a
David Jenkins <pikolin@gmail.com>
2023-04-13 17:21:24 +0200

    Setting up Nativewind and linter

--- App.tsx
+++ App.tsx
@@ -1,20 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { Text, SafeAreaView } from 'react-native';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+	return (
+		<SafeAreaView className="flex-1 items-center justify-center bg-red-600">
+			<Text>Open up App.js to start working on your app!</Text>
+			<StatusBar style="auto" />
+		</SafeAreaView>
+	);
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
